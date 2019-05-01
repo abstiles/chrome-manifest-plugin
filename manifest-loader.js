5e66c6985c15441dcc620cfa3ae74da7f16250ee
CiriousJoker <ciriousjoker@gmail.com>
2019-05-01 10:53:13 +0200

    Bugfix: package.json overwrote manifest.json

--- manifest-loader.js
+++ manifest-loader.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 module.exports = function(source) {
   let options = loaderUtils.getOptions(this);
   const pkg = JSON.parse(fs.readFileSync('./package.json'));
-  const merged = Object.assign({}, JSON.parse(source), {
+  const merged = Object.assign({}, {
     'manifest_version': 2,
     'name': pkg.name,
     'description': pkg.description,
     'version': pkg.version,
     'author': pkg.author,
     'homepage_url': pkg.homepage,
-  });
+  }, JSON.parse(source));
   const mergedJson = JSON.stringify(merged);
   this.emitFile('manifest.json', mergedJson);
   return mergedJson;
